d8a17705a37a7095d51bb3d6f6fa9ae110b58efd
Winbobob <zhu6@ncsu.edu>
2021-01-10 11:12:21 -0800

    Change command naming: approve (disputes) -> cancel; reject (disputes) -> confirm

--- lib/danger/helpers/comments_helper.rb
+++ lib/danger/helpers/comments_helper.rb
@@ -68,7 +68,7 @@ module Danger
         resolved_violations = previous_violations.reject do |pv|
           content.count { |v| messages_are_equivalent(v, pv) } > 0
         end
-        # 2. violations approved or rejected by teaching staff 
+        # 2. violations caneled or confirmed by teaching staff
         previous_violations_dict = {} # key: UUID, value: violation object 
         previous_violations.each do |pv|
           # Remove html tags
@@ -79,17 +79,17 @@ module Danger
         end
 
         issue_comments.each do |issue_comment|
-          next if !issue_comment.body.start_with?("/approve") and !issue_comment.body.start_with?("/reject")
+          next if !issue_comment.body.start_with?("/cancel") and !issue_comment.body.start_with?("/confirm")
           # Split one or more whitespaces and commas
           issue_comment_arr = issue_comment.body.split(/[\s,]+/)
           puts "issue_comment_arr: " + issue_comment_arr.to_s
           issue_comment_arr[1..-1].each do |uuid|
             pv = previous_violations_dict[uuid]
             next if not pv
-            if issue_comment_arr[0] == "/approve"
+            if issue_comment_arr[0] == "/cancel"
               resolved_violations << pv if !resolved_violations.include? pv
               content.reject!{ |v| messages_are_equivalent(v, pv) }
-            elsif issue_comment_arr[0] == "/reject"
+            elsif issue_comment_arr[0] == "/confirm"
               content << pv if !content.include? pv
               resolved_violations.reject!{ |v| messages_are_equivalent(v, pv) }
             end

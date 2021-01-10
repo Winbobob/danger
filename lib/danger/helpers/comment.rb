1ccfed23f809a1e5c46e2533cfd7a3138f6cd821
Winbobob <zhu6@ncsu.edu>
2021-01-10 12:31:57 -0800

    Fix 10a2cce17044bc6e1e0caecc3295259ddd6171b8

--- lib/danger/helpers/comment.rb
+++ lib/danger/helpers/comment.rb
@@ -1,21 +1,22 @@
 module Danger
   class Comment
-    attr_reader :id, :body
+    attr_reader :id, :author, :body
 
-    def initialize(id, body)
+    def initialize(id, author, body)
       @id = id
+      @author = author
       @body = body
     end
 
     def self.from_github(comment)
-      self.new(comment["id"], comment["body"])
+      self.new(comment["id"], comment["user"]["login"], comment["body"])
     end
 
     def self.from_gitlab(comment)
       if comment.respond_to?(:id) && comment.respond_to?(:body)
         self.new(comment.id, comment.body)
       else
-        self.new(comment["id"], comment["body"])
+        self.new(comment["id"], comment["user"]["login"], comment["body"])
       end
     end
 
